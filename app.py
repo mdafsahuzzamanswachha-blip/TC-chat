785afb10845b1b7e90685c346a8b443acb86a252
mdafsahuzzamanswachha-blip <mdafsahuzzamanswachha@gmail.com>
2025-09-01 19:01:51 +0600

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def handle_signal(data):
 # Main entry point to run the application.
 if __name__ == '__main__':
     eventlet.monkey_patch()
-    socketio.run(app, debug=True, port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
